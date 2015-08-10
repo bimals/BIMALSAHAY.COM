1f2c7e6fbd38468a7448d485da835b5b9dbced2a
Bimal Sahay <bimal.sahay@getinsured.com>
2015-08-09 21:33:08 -0700

    Batch

---
+++ batch/src/test/java/com/bimalsahay/batch/ExampleItemWriterTests.java
@@ -0,0 +1,15 @@
+package com.bimalsahay.batch;
+
+import com.bimalsahay.batch.ExampleItemWriter;
+
+import junit.framework.TestCase;
+
+public class ExampleItemWriterTests extends TestCase {
+
+	private ExampleItemWriter writer = new ExampleItemWriter();
+	
+	public void testWrite() throws Exception {
+		writer.write(null); // nothing bad happens
+	}
+
+}
