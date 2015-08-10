a0990ac8837029559354bbf6ceda1d75974b06fe
Bimal Sahay <bimal.sahay@getinsured.com>
2015-08-09 21:06:16 -0700

    Fixed

--- blog/src/main/java/blog/platform/authentication/CustomUserDetailsService.java
+++ blog/src/main/java/blog/platform/authentication/CustomUserDetailsService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.Authentication;
@@ -16,11 +15,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.getinsured.hix.model.UserRole;
-import com.getinsured.hix.model.UserSession;
-import com.getinsured.hix.platform.security.repository.IUserRepository;
-import com.getinsured.hix.platform.security.repository.IUserSessionRepository;
-import com.getinsured.hix.platform.security.session.SessionTrackerService;
 
 import blog.model.AccountUser;
 
@@ -28,62 +22,18 @@ import blog.model.AccountUser;
 @Service
 @Transactional(readOnly = true)
 public class CustomUserDetailsService implements UserDetailsService,AuthenticationUserDetailsService {
-	private static final Logger LOGGER = Logger.getLogger(CustomUserDetailsService.class);
+	//private static final Logger LOGGER = Logger.getLogger(CustomUserDetailsService.class);
 	
-	@Autowired private IUserRepository userRepository;
-	@Autowired private IUserSessionRepository sessionRepo;
-	@Autowired private SessionTrackerService sessionTracker;
 		
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		LOGGER.info("Getting user details from loadUserByUsername :: " );
-		
 		AccountUser domainUser = null;
-		List<UserSession> invalidatedSessions = null;
-		try {
-			
-			if(username.contains("ssoAuth:")){
-				username=StringUtils.substringAfter(username, "ssoAuth:");
-				domainUser = userRepository.findByExtnAppUserId(username);
-				domainUser.setPassword("ssoAuth:NOT_APPLICABLE");
-				domainUser.setUuid("ssoAuth:NOT_APPLICABLE");
-				
-			}else if(username.equalsIgnoreCase("anonymous")) {
-				domainUser = new AccountUser();
-				domainUser.setUserName("anonymous");
-				domainUser.setAuthenticated(true);
-				domainUser.setConfirmed(1);
-			}
-			else {
-				domainUser = userRepository.findByUserName(username.toLowerCase());
-			}
-			
-			
-			if (domainUser == null) {
-				LOGGER.error("Username Not Found :: " + username);
-	            //throw new UsernameNotFoundException(username);
-	        }else{
-	        	List<UserSession> existingSessions = this.sessionRepo.findActiveUserSession(domainUser.getId());
-	        	if(existingSessions != null && existingSessions.size() > 0){
-	        		// Check if multiple Sessions are allowed
-	        		if(!this.sessionTracker.checkSessionMultiplicityAllowed(this.isPrivilegedUser(domainUser))){
-	        			invalidatedSessions = this.sessionTracker.invalidateAllSessions(existingSessions);
-	        			if(LOGGER.isInfoEnabled()){
-	        				LOGGER.info("Invalidated existing session ["+invalidatedSessions.size()+"] for user "+domainUser.getId());
-	        			}
-	        		}
-	        	}
-	        }
-		} catch (Exception ex) {
-        	LOGGER.error("Exception : " + ex.getMessage(),ex);
-            throw new AuthenticationServiceException(ex.getMessage(), ex);
-        }
 		return domainUser;
 	}
 
 	@Override
 	public UserDetails loadUserDetails(Authentication token)
 			throws UsernameNotFoundException {
-		LOGGER.info("Getting user details from loadUserDetails :: " );
+		//LOGGER.info("Getting user details from loadUserDetails :: " );
 		
 		String username = token.getName();
 		UserDetails domainUser = null;
@@ -94,9 +44,9 @@ public class CustomUserDetailsService implements UserDetailsService,Authenticati
 	}
 	
 	private boolean isPrivilegedUser(AccountUser user){
-		Set<UserRole> userRoles = user.getUserRole();
+		//Set<UserRole> userRoles = user.getUserRole();
 		boolean isPrivilegedRole = false;
-		UserRole tmp = null;
+/*		UserRole tmp = null;
 		Iterator<UserRole> cursor = userRoles.iterator();
 		while(cursor.hasNext()){
 			tmp = cursor.next();
@@ -106,7 +56,7 @@ public class CustomUserDetailsService implements UserDetailsService,Authenticati
 					break;
 				}
 			}
-		}
+		}*/
 		return isPrivilegedRole;
 	}
 	
